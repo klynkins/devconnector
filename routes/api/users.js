0fd755ecaab2eb8797c0ae9e9e978a1bf52ac999
Katelynn Campbell <katelynncampbell@Katelynns-MacBook.local>
2018-09-10 21:14:58 -0400

    models

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+// Load User Model
+const User = require("../../models/User");
+
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+
+// @route   GET api/users/register
+// @desc    Register users
+// @access  Public
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      returnres.status(400).json({ email: "Email already exists" });
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
